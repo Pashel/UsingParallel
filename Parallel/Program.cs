565b49ac037c6690f61a8c31e65090f0caf7dc75
Pavel Sannikau <Pavel_Sannikau@epam.com>
2017-10-18 10:46:30 +0300

    add matrix examples to source conrol

--- Parallel/Program.cs
+++ Parallel/Program.cs
@@ -7,13 +7,13 @@ namespace ParallelProject
         static void Main(string[] args)
         {
             var provider = new FileMatrixProvider();
-            var matrix1 = provider.GetMatrix(@"1.txt");
-            var matrix2 = provider.GetMatrix(@"2.txt");
+            var matrix1 = provider.GetMatrix(@"../../1.txt");
+            var matrix2 = provider.GetMatrix(@"../../2.txt");
 
             var manager = new MatrixOperations();
             var result = manager.Multiply(matrix1, matrix2);
 
-            provider.SaveMatrix(@"3.txt", result);
+            provider.SaveMatrix(@"../../3.txt", result);
             
             Console.WriteLine("Done. Please press any key...");
             Console.ReadLine();
